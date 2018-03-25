b91a6b351a1ea8372d292e19ef8cfb4175c9d959
petrarch1603 <petrarch1603@gmail.com>
2018-03-24 19:24:25 -0600

    PEP compliance sweep

--- PhotoKML.py
+++ PhotoKML.py
@@ -1,8 +1,8 @@
 import datetime
+import os
 import PIL.Image, PIL.ExifTags
 import shutil
 import xml.dom.minidom
-import os
 
 dir = "photos/"
 now = datetime.datetime.now()
@@ -22,7 +22,6 @@ def make_KMZ_dir(new_kml_dir):
     return today_dir
 
 
-
 def get_exif_data(file):
     img = PIL.Image.open(file)
     exif = {
@@ -32,14 +31,16 @@ def get_exif_data(file):
     }
     return exif
 
+
 def get_raw_gps_data(file):
     raw_exif = get_exif_data(file)
     orientation = raw_exif['Orientation']
-    rawgpsdata = {"Orientation": orientation}
+    raw_gps_data = {"Orientation": orientation}
     for key in raw_exif['GPSInfo'].keys():
         decode = PIL.ExifTags.GPSTAGS.get(key, key)
-        rawgpsdata[decode] = raw_exif['GPSInfo'][key]
-    return rawgpsdata
+        raw_gps_data[decode] = raw_exif['GPSInfo'][key]
+    return raw_gps_data
+
 
 def convert_to_degrees(value):
     d0 = value[0][0]
@@ -58,32 +59,33 @@ def convert_to_degrees(value):
 
 
 def get_gps_data(file):
-    rawgpsdata = get_raw_gps_data(file)
-    rawLat = convert_to_degrees(value=rawgpsdata['GPSLatitude'])
-    rawLong = convert_to_degrees(value=rawgpsdata['GPSLongitude'])
-    orientation = rawgpsdata['Orientation']
+    raw_gps_data = get_raw_gps_data(file)
+    raw_lat = convert_to_degrees(value=raw_gps_data['GPSLatitude'])
+    raw_long = convert_to_degrees(value=raw_gps_data['GPSLongitude'])
+    orientation = raw_gps_data['Orientation']
     try:
-        altitude = rawgpsdata['GPSAltitude'][0] / rawgpsdata['GPSAltitude'][1]
+        altitude = raw_gps_data['GPSAltitude'][0] / raw_gps_data['GPSAltitude'][1]
     except KeyError:
         altitude = 0
 
-    if rawgpsdata['GPSLatitudeRef'] == 'S':
-        Lat = (-(rawLat))
+    if raw_gps_data['GPSLatitudeRef'] == 'S':
+        lat = -raw_lat
     else:
-        Lat = rawLat
-    if rawgpsdata['GPSLongitudeRef'] == 'W':
-        Long = (-(rawLong))
+        lat = raw_lat
+    if raw_gps_data['GPSLongitudeRef'] == 'W':
+        long = -raw_long
     else:
-        Long = rawLong
-    direction = (rawgpsdata['GPSImgDirection'][0] / rawgpsdata['GPSImgDirection'][1])
+        long = raw_long
+    direction = (raw_gps_data['GPSImgDirection'][0] / raw_gps_data['GPSImgDirection'][1])
     clean_gps = {
-        "Longitude": Long,
-        "Latitude": Lat,
+        "Longitude": long,
+        "Latitude": lat,
         "Altitude": altitude,
         "Direction": direction,
         "Orientation": orientation
     }
     return clean_gps
+
 
 def create_kml_doc():
     kml_doc = xml.dom.minidom.Document()
@@ -95,19 +97,19 @@ def create_kml_doc():
     return kml_doc
 
 
-def photo_overlay(kml_doc, clean_gps, photofilepath):
+def photo_overlay(kml_doc, clean_gps, photo_file_path):
     po = kml_doc.createElement('PhotoOverlay')
     name = kml_doc.createElement('name')
-    name.appendChild(kml_doc.createTextNode(photofilepath))
+    name.appendChild(kml_doc.createTextNode(photo_file_path))
     description = kml_doc.createElement('description')
     description.appendChild(kml_doc.createCDATASection('<a href="#%s">'
                                                        'Click here to fly into '
-                                                       'photo</a>' % photofilepath))
+                                                       'photo</a>' % photo_file_path))
     po.appendChild(name)
     po.appendChild(description)
     icon = kml_doc.createElement('Icon')
     href = kml_doc.createElement('href')
-    href.appendChild(kml_doc.createTextNode(photofilepath))
+    href.appendChild(kml_doc.createTextNode(photo_file_path))
     camera = kml_doc.createElement('Camera')
     longitude = kml_doc.createElement('longitude')
     latitude = kml_doc.createElement('latitude')
@@ -116,18 +118,16 @@ def photo_overlay(kml_doc, clean_gps, photofilepath):
     heading = kml_doc.createElement('heading')
 
     # Set the Field of View
-    width = get_exif_data(photofilepath)['ExifImageWidth']
-    length = get_exif_data(photofilepath)['ExifImageHeight']
     # The following might seem counter-intuitive.
     # If the photo has been rotated, we need to switch the height and width
     # Otherwise Google Earth will stretch the image as if it is still in the landscape mode.
     orient = clean_gps['Orientation']
     if orient in (6, 8):
-        width = get_exif_data(photofilepath)['ExifImageHeight']
-        length = get_exif_data(photofilepath)['ExifImageWidth']
+        width = get_exif_data(photo_file_path)['ExifImageHeight']
+        length = get_exif_data(photo_file_path)['ExifImageWidth']
     else:
-        width = get_exif_data(photofilepath)['ExifImageWidth']
-        length = get_exif_data(photofilepath)['ExifImageHeight']
+        width = get_exif_data(photo_file_path)['ExifImageWidth']
+        length = get_exif_data(photo_file_path)['ExifImageHeight']
     lf = str(width / length * -20.0)
     rf = str(width / length * 20.0)
 
@@ -142,22 +142,22 @@ def photo_overlay(kml_doc, clean_gps, photofilepath):
     camera.appendChild(tilt)
     camera.appendChild(heading)
     icon.appendChild(href)
-    viewvolume = kml_doc.createElement('ViewVolume')
-    leftfov = kml_doc.createElement('leftFov')
-    rightfov = kml_doc.createElement('rightFov')
-    bottomfov = kml_doc.createElement('bottomFov')
-    topfov = kml_doc.createElement('topFov')
+    view_volume = kml_doc.createElement('ViewVolume')
+    left_fov = kml_doc.createElement('leftFov')
+    right_fov = kml_doc.createElement('rightFov')
+    bottom_fov = kml_doc.createElement('bottomFov')
+    top_fov = kml_doc.createElement('topFov')
     near = kml_doc.createElement('near')
-    leftfov.appendChild(kml_doc.createTextNode(lf))
-    rightfov.appendChild(kml_doc.createTextNode(rf))
-    bottomfov.appendChild(kml_doc.createTextNode('-20'))
-    topfov.appendChild(kml_doc.createTextNode('20'))
+    left_fov.appendChild(kml_doc.createTextNode(lf))
+    right_fov.appendChild(kml_doc.createTextNode(rf))
+    bottom_fov.appendChild(kml_doc.createTextNode('-20'))
+    top_fov.appendChild(kml_doc.createTextNode('20'))
     near.appendChild(kml_doc.createTextNode('10'))
-    viewvolume.appendChild(leftfov)
-    viewvolume.appendChild(rightfov)
-    viewvolume.appendChild(bottomfov)
-    viewvolume.appendChild(topfov)
-    viewvolume.appendChild(near)
+    view_volume.appendChild(left_fov)
+    view_volume.appendChild(right_fov)
+    view_volume.appendChild(bottom_fov)
+    view_volume.appendChild(top_fov)
+    view_volume.appendChild(near)
     style = kml_doc.createElement('Style')
     iconstyle = kml_doc.createElement('IconStyle')
     # The following are elements for the style only.
@@ -171,7 +171,7 @@ def photo_overlay(kml_doc, clean_gps, photofilepath):
     po.appendChild(style)
     po.appendChild(camera)
     po.appendChild(icon)
-    po.appendChild(viewvolume)
+    po.appendChild(view_volume)
     point = kml_doc.createElement('Point')
     coordinates = kml_doc.createElement('coordinates')
     coordinates.appendChild(kml_doc.createTextNode('%s,%s,%s' % (clean_gps['Longitude'],
@@ -183,17 +183,18 @@ def photo_overlay(kml_doc, clean_gps, photofilepath):
     document.appendChild(po)
 
 
-def CreateKmlFile(dir, new_kml_name):
+def Create_Kml_File(dir, new_kml_name):
     kml_doc = create_kml_doc()
-    for photofile in os.listdir(dir):
-        if not photofile.startswith('.'):
-            photofilepath = (dir + photofile)
-            process_photo(photofilepath)
-            clean_gps = get_gps_data(photofilepath)
-            photo_overlay(kml_doc, clean_gps, photofilepath)
+    for photo_file in os.listdir(dir):
+        if not photo_file.startswith('.'):
+            photo_file_path = (dir + photo_file)
+            process_photo(photo_file_path)
+            clean_gps = get_gps_data(photo_file_path)
+            photo_overlay(kml_doc, clean_gps, photo_file_path)
     kml_file = open(new_kml_name, 'w')
     kml_file.write(kml_doc.toprettyxml())
     print(kml_doc.toprettyxml())
+
 
 def process_photo(file):
     try:
@@ -222,14 +223,10 @@ def main(dir):
     today_dir = make_KMZ_dir(today)
     home_dir = os.getcwd()
     os.chdir(today_dir)
-    CreateKmlFile('files/', new_kml_name=new_kml_name)
+    Create_Kml_File('files/', new_kml_name=new_kml_name)
     os.chdir(home_dir)
     shutil.make_archive(base_name=(today + '.kmz'), format='zip', root_dir=today_dir)
     os.rename(today + '.kmz.zip', today + '.kmz')
     shutil.rmtree(today_dir)
 
 main(dir)
-#print(get_raw_gps_data(file))
-#x = get_exif_data(file)
-#print(x['Orientation'])
-
